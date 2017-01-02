9f57d33522afae14dfc07325669b933d7f2e84ec
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2017-01-02 11:31:37 +0100

    Search form template

---
+++ source/php/SearchForm.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace WpListings;
+
+class SearchForm
+{
+    public function __construct()
+    {
+        // Add shortcode
+        add_shortcode('wp-listings-search-form', array($this, 'showForm'));
+    }
+
+    /**
+     * Shows the frontend form
+     * @return void
+     */
+    public function showForm()
+    {
+        $template = apply_filters('wp-listings/search_template', WPLISTINGS_TEMPLATE_PATH . '/search-form.php');
+        include_once $template;
+    }
+}
---
+++ templates/search-form.php
@@ -0,0 +1,11 @@
+<div class="wp-listings-search">
+    <form action="" method="get">
+        <div class="grid">
+            <div class="grid-md-12">
+                <div class="form-group">
+                    <input type="search" name="s" placeholder="<?php __('Search'); ?>">
+                </div>
+            </div>
+        </div>
+    </form>
+</div>
