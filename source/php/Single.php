4f22ed705f2d71da84a9a426b20578e116aac004
Sebastian Thulin <sebastian.thulin@gmail.com>
2017-01-23 14:34:08 +0100

    Added label change.

--- source/php/Single.php
+++ source/php/Single.php
@@ -9,20 +9,30 @@ class Single
 
     public function __construct()
     {
-        add_filter( 'the_title', array($this, 'addGoBackButton'), 10, 2 );
+        add_filter('the_title', array($this, 'addGoBackButton'), 10, 2);
     }
 
-    public function addGoBackButton ($title, $post_id = null) : string {
-        if($this->isSingleListing($post_id) && !$this->buttonPrinted) {
+    public function addGoBackButton($title, $post_id = null) : string
+    {
+        if ($this->isSingleListing($post_id) && !$this->buttonPrinted) {
+
+            //Do not show again
             $this->buttonPrinted = true;
-            return $title . "" . '<a href="' . get_post_type_archive_link(\WpListings\Listings::$postTypeSlug) . '" class="btn btn-sm pull-right" style="font-size: 15px">' . __("Back to listing", 'wp-listings') . '</a>';
+
+            //Get label
+            $label = !empty(get_field('listing_back_button_label', 'option')) ? get_field('listing_back_button_label', 'option') : __("Back to listing", 'wp-listings');
+
+            //Return button markup
+            return $title . "" . '<a href="' . get_post_type_archive_link(\WpListings\Listings::$postTypeSlug) . '" class="btn btn-sm pull-right" style="font-size: 15px">' . $label . '</a>';
+
         }
         return $title;
     }
 
-    private function isSingleListing ($post_id) : bool {
+    private function isSingleListing($post_id) : bool
+    {
         global $wp_query;
-        if(!is_archive() && is_numeric($post_id) && is_single($post_id) && get_post_type($post_id) == \WpListings\Listings::$postTypeSlug && $wp_query->is_main_query()) {
+        if (!is_archive() && is_numeric($post_id) && is_single($post_id) && get_post_type($post_id) == \WpListings\Listings::$postTypeSlug && $wp_query->is_main_query()) {
             return true;
         }
         return false;
